058af877b1f70d3063d276bb50e54fdb6da8eea7
Ben <benjaminjmcgaw@gmail.com>
2019-09-04 07:44:25 +1000

    Finisheed Website Integration

--- server.js
+++ server.js
@@ -4,26 +4,22 @@
 
 const express = require('express');
 const app = express();
-app.set('view engine', 'pug');
-var path = require('path');
+app.set('view engine', 'pug');                      //Set the render engine
+var path = require('path');                         //Honestly Not Sure :P
+app.use(express.static(__dirname + '/public'));   //Set the /images folder to be publicly viewable
+//console.log(`Made Directory: ${__dirname}\\images`);
 
 //Log the port to the console
 const server = app.listen(12983, () => {
     console.log(`Express running → PORT ${server.address().port}`);
 });
 
-// MongoClient.connect(url, function(err, db) {
-//     if (err) throw err;
-//     var dbo = db.db("mydb");
-//     dbo.createCollection("customers", function(err, res) {
-//         if (err) throw err;
-//         console.log("Collection created!");
-//         db.close();
-//     });
-// });
-
-//Load the webpages
+//Set up Dir Routes
 app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/' + 'index.html'));
+})
+
+app.get('/about', function (req, res) {
     res.sendFile(path.join(__dirname + '/views/' + 'about.html'));
 })
 
